972893c862500c2c4ad6d22a2e914cb3fc52383d
tamsay <longetemitope@gmail.com>
2020-03-28 18:38:20 +0100

    First build file of the smartbrain app - frontend

---
+++ precache-manifest.5f173bcd4b1ea2edf012fdfdd060fe71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "078354f919c788e0829a015dbab3259f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed137f6a226a755f1209",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fae6c2f9970edd882c58",
+    "url": "/static/js/2.a90af8c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a90af8c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed137f6a226a755f1209",
+    "url": "/static/js/main.3bf8b23f.chunk.js"
+  },
+  {
+    "revision": "c1aa80a2326505a838ea",
+    "url": "/static/js/runtime-main.79f36662.js"
+  }
+]);
